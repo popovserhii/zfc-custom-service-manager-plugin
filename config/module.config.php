8ac1519d3e64b1e95aa69e8d32d6c3d3a4fa29ed
Sergiy Popov <popovsergiy@users.noreply.github.com>
2016-02-21 02:55:30 +0200

    Update module.config.php

--- config/module.config.php
+++ config/module.config.php
@@ -3,12 +3,6 @@ namespace Ageme\Project;
 
 return [
 
-	'service_manager' => [
-		'factories' => [
-			'ProjectPluginManager' => 'Ageme\Project\Service\Plugin\ProjectPluginFactory',
-		],
-	],
-
 	'project_plugins' => [
 		'invokables' => [
 			'web-app' => 'Ageme\Project\Service\Plugin\WebApp',
